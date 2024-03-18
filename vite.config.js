d2e3bfebbfa7e73a58621abbacd890a255efabe6
Jason Lee <jason@zerodevx.com>
2024-03-18 21:08:37 +0800

    Fix build config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    copyPublicDir: false
+  },
   plugins: [svelte()]
 })
