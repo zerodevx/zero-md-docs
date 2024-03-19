19343e43b538b885cca2105a194c19b5cb3e5eb3
Jason Lee <jason@zerodevx.com>
2024-03-19 17:29:35 +0800

    Refactor styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,71 +1,66 @@
 <script>
 ZeroMdDocsConfig = ZeroMdDocsConfig || { links: [['Home', 'readme.md']] }
 const { links } = ZeroMdDocsConfig
-const basePath = location.origin + location.pathname
+const baseUrl = location.origin + location.pathname
 
 function setSrc() {
   const url = new URL(location.href)
-  const md = url.searchParams.get('p') || 'readme.md'
-  const entry = links.find((i) => i[1] === md)
+  const file = url.searchParams.get('p') || 'readme.md'
+  const entry = links.find((i) => i[1] === file)
   if (entry) {
-    const [title, md, altTitle] = entry
-    document.title = altTitle || title
-    src = md
+    const [tit, file, alt] = entry
+    title = alt || tit || 'Home'
+    src = file
   } else {
-    document.title = '404'
+    title = '404'
     src = ''
   }
 }
 
 function clicked(e) {
   const a = e.composedPath()[0].closest('a')
-  if (a && a.href.startsWith(basePath) && a.href.endsWith('.md')) {
+  if (a && a.href.startsWith(baseUrl) && a.href.endsWith('.md')) {
     e.preventDefault()
     const url = new URL(location.href)
-    const md = a.href.split('/').pop()
-    md === 'readme.md' ? url.searchParams.delete('p') : url.searchParams.set('p', md)
-    const title = e.target.getAttribute('data-title') || e.target.text
-    document.title = title
+    const file = a.href.split('/').pop()
+    file === 'readme.md' ? url.searchParams.delete('p') : url.searchParams.set('p', file)
+    title = e.target.getAttribute('data-title') || e.target.text
     history.pushState({ title }, '', url.toString())
-    src = md
     scrollTo(0, 0)
+    src = file
   }
 }
 
+let title = ''
 let src = ''
 setSrc()
 </script>
 
 <svelte:window on:popstate={setSrc} on:click={clicked} />
 
-<div class="relative mx-auto max-w-screen-xl px-2 pb-12 pt-2">
-  <ul
-    class="mb-2 w-full rounded border border-gray-200 bg-white md:float-right lg:sticky lg:top-0 lg:w-[256px] dark:border-gray-800 dark:bg-gray-950 dark:text-gray-100"
-  >
-    {#each links as [title, href, altTitle]}
-      <li class="h-10">
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<div class="zmdocs-container">
+  <ul class="zmdocs-menu">
+    {#each links as [title, href, alt]}
+      <li>
         <a
-          class="flex h-full w-full items-center justify-center px-4 text-sm hover:bg-blue-100 lg:justify-start dark:hover:bg-blue-800"
+          class="zmdocs-link"
           class:font-bold={href.split('/').pop() === src}
-          data-title={altTitle}
+          data-title={alt}
           {href}>{title}</a
         >
       </li>
     {/each}
   </ul>
-  <div class="w-full lg:pr-[264px]">
-    <zero-md class="w-full rounded border border-gray-200 dark:border-gray-800" {src}>
+  <div class="zmdocs-content">
+    <zero-md {src}>
       <template data-append>
+        <!-- prettier-ignore -->
         <style>
-        .markdown-body {
-          box-sizing: border-box;
-          padding: 45px;
-        }
-        @media (max-width: 767px) {
-          .markdown-body {
-            padding: 15px;
-          }
-        }
+.markdown-body{box-sizing:border-box;padding:45px;}@media(max-width:767px){.markdown-body{padding:15px;}}
         </style>
       </template>
       <!-- prettier-ignore -->
